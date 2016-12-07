8647c524c4e8f9a6ee101998e1d96f36ec9d145f
William of Ockham <certainthings@gmail.com>
2016-12-07 16:02:52 +1100

    array example

---
+++ 01-javascript/arrays/js/exercise1.js
@@ -0,0 +1,13 @@
+/*
+Your top choices
+Create an array to hold your top choices (colors, presidents, whatever).
+For each choice, log to the screen a string like: "My #1 choice is blue."
+
+Bonus: Change it to log "My 1st choice, "My 2nd choice", "My 3rd choice", picking the right suffix for the number based on what it is.
+*/
+
+var presidents = ['Lincoln', 'Garfield', 'Cleveland', 'Obama', 'Carter'];
+
+for (var i = 0; i < presidents.length; i++) {
+  console.log("My #" + (i + 1) + " choice is " + presidents[i] + ".");
+}
