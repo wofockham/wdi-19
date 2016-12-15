b45de7fe530fe099a70ac942fcdf8d54769696f0
William of Ockham <certainthings@gmail.com>
2016-12-15 14:36:34 +1100

    now with .each

--- 03-jquery/youtube-ii/js/videos.js
+++ 03-jquery/youtube-ii/js/videos.js
@@ -1,13 +1,13 @@
-var links = $('ul a');
+var $links = $('ul a');
 
-for (var i = 0; i < links.length; i++) {
-  var $l = links.eq(i); // Retrieve the jQuery version of the current link.
+$links.each(function () {
+    var $l = $( this ); // Retrieve the jQuery version of the current link.
 
-  var href = $l.attr('href'); // Using .attr() as a getter.
+    var href = $l.attr('href'); // Using .attr() as a getter.
 
-  var thumbnailURL = youtube.generateThumbnailUrl( href );
-  var $img = $('<img>');
-  $img.attr('src', thumbnailURL); // Using .attr() as a setter.
+    var thumbnailURL = youtube.generateThumbnailUrl( href );
+    var $img = $('<img>');
+    $img.attr('src', thumbnailURL); // Using .attr() as a setter.
 
-  $l.append($img);
-}
+    $l.append($img);
+});
