e8437f98ebc6810e6564fb3cf55c632f758bcf4a
William of Ockham <certainthings@gmail.com>
2017-01-12 10:55:43 +1100

    sql god i hate it

---
+++ 06-databases/sql_intro/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO people (id, name, age) VALUES (2, 'Craigsy', 17);
+INSERT INTO people VALUES (3, 'Jorge', 88);
+
+INSERT INTO pets (id, name, breed, age, dead) VALUES (1, 'Fluffy', 'Unicorn', 1000, 0);
+INSERT INTO pets VALUES (2, 'Gigantor', 'Robot', 1, 1);
---
+++ 06-databases/sql_intro/people.sql
@@ -0,0 +1,5 @@
+CREATE TABLE people (
+  id INTEGER PRIMARY KEY,
+  name TEXT,
+  age INTEGER
+);
