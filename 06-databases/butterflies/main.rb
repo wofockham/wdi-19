1c36e02e2869a4b46c00222b39e7df94a949902f
William of Ockham <certainthings@gmail.com>
2017-01-12 12:59:36 +1100

    butterflies CRUD creation

--- 06-databases/butterflies/main.rb
+++ 06-databases/butterflies/main.rb
@@ -8,23 +8,37 @@ end
 
 # INDEX
 get '/butterflies' do
-  db = SQLite3::Database.new 'database.sqlite3'
-  db.results_as_hash = true
-
-  @butterflies = db.execute 'SELECT * FROM butterflies'
-
+  @butterflies = query_db 'SELECT * FROM butterflies'
   erb :butterflies_index
+end
+
+# NEW
+get '/butterflies/new' do
+  erb :butterflies_new
+end
+
+# CREATE
+post '/butterflies' do
+  query_db "INSERT INTO butterflies (name, family, image) VALUES ('#{params[:name]}', '#{params[:family]}', '#{params[:image]}')"
+  redirect to('/butterflies')
 end
 
 # SHOW
 get '/butterflies/:id' do
-  # Get a specific from the database
-  db = SQLite3::Database.new 'database.sqlite3'
-  db.results_as_hash = true
-
-  @butterfly = db.execute "SELECT * FROM butterflies WHERE id = #{ params[:id] }"
+  @butterfly = query_db "SELECT * FROM butterflies WHERE id = #{ params[:id] }"
   @butterfly = @butterfly.first # Pluck the sole butterfly from the array
 
   # Show some HTML
   erb :butterflies_show
+end
+
+def query_db(sql)
+  db = SQLite3::Database.new 'database.sqlite3'
+  db.results_as_hash = true
+
+  puts sql # Optional but nice to have
+
+  results = db.execute sql
+  db.close
+  results # Implicit return
 end
