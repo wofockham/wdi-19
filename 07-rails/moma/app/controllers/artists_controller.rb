20c4739befd5679eff15eacfaa64f6f99f186609
William of Ockham <certainthings@gmail.com>
2017-01-18 10:59:13 +1100

    moma artists#index

---
+++ 07-rails/moma/app/controllers/artists_controller.rb
@@ -0,0 +1,23 @@
+class ArtistsController < ApplicationController
+  def index
+    @artists = Artist.all
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def show
+  end
+
+  def destroy
+  end
+end
