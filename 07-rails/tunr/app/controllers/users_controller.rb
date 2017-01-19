785010b143d2caabe4d6752b7190cdb35e0bc8f7
William of Ockham <certainthings@gmail.com>
2017-01-20 09:46:34 +1100

    sign in on sign up

--- 07-rails/tunr/app/controllers/users_controller.rb
+++ 07-rails/tunr/app/controllers/users_controller.rb
@@ -6,6 +6,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new user_params
     if @user.save # Checking for validity
+      session[:user_id] = @user.id # Signing up also signs you in.
       redirect_to root_path
     else
       render :new
