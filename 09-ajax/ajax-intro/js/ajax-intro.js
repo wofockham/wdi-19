0b0ca1fe74e77593e3db215c5b8a491fa3629682
William of Ockham <certainthings@gmail.com>
2017-01-30 14:01:03 +1100

    annotated AJAX

--- 09-ajax/ajax-intro/js/ajax-intro.js
+++ 09-ajax/ajax-intro/js/ajax-intro.js
@@ -1,16 +1,21 @@
-var xhr = new XMLHttpRequest(); // A Javascript web browser.
+var xhr = new XMLHttpRequest(); // Like HTTParty for JS
+
+// To process the results of an asynchronous function like xhr.send()
+// we MUST use a callback to wait for the request to complete.
 xhr.onreadystatechange = function () {
-  if (xhr.readyState < 4) { return; } // Request is still in progress, so abort.
+  // If the request is still in progress, do nothing.
+  if (xhr.readyState < 4) { return; }
+
   var movieInfo = xhr.responseText;
-  var poster = movieInfo['Poster'];
   console.log(movieInfo);
+  var poster = movieInfo['Poster'];
   console.log(poster);
-}
-xhr.open('GET', 'http://omdbapi.com/?t=jaws');
-xhr.send(); // Asynchronous
+};
 
+xhr.open('GET', 'http://omdbapi.com/?t=jaws'); // Set up
+xhr.send(); // Asynchronous call!
 
-// Wrong approach
+// Wrong approach: we can't predict how long the request will take
 // setTimeout(function () {
 //   console.log( xhr.responseText );
 // }, 3000);
