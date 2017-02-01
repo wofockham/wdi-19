9ccf96ab81aab6161673a4bd7d6721f3709167ea
William of Ockham <certainthings@gmail.com>
2017-02-01 12:02:16 +1100

    underscore intro

--- 10-underscore/intro/js/intro.js
+++ 10-underscore/intro/js/intro.js
@@ -24,6 +24,7 @@ _(groucho).each(function (value, key) {
   console.log(key, 'is', value);
 });
 
+
 // MAP /////////////////////////////////////////////////////////////////////////
 var fullBrothers = _(bros).map(function (b, index, entireArray) {
   return '<h1>' + b + ' Marx' + '</h1>';
@@ -45,3 +46,53 @@ var total = _(nums).reduce(function (sum, n) { // sum => accumulator
 });
 
 console.log(total);
+
+
+// FIND ////////////////////////////////////////////////////////////////////////
+var nums = [1, 1, 1, 5, 7, 11, 14, 7, 3, 20, 8, 5, 17];
+var result = _(nums).find(function (n) {
+  return n % 2 === 0; // Is n even?
+});
+
+console.log(result);
+
+var brother = _(bros).find(function (b) {
+  return b.includes('i');
+});
+console.log(brother);
+
+// FILTER //////////////////////////////////////////////////////////////////////
+// REJECT //////////////////////////////////////////////////////////////////////
+var evens = _(nums).filter(function (n) {
+  return n % 2 === 0; // Is n even?
+});
+console.log( evens );
+
+// WHERE ///////////////////////////////////////////////////////////////////////
+var actors = [
+  {name: 'Groucho', gender: 'male'},
+  {name: 'Harpo', gender: 'male'},
+  {name: 'Mrs Dumont', gender: 'female'},
+  {name: 'Chico', gender: 'male'}
+];
+
+// var maleActors = _(actors).filter(function (a) {
+//   return a.gender === 'male';
+// });
+var maleActors = _(actors).where( {gender: 'male'} );
+var femaleActors = _(actors).where( {gender: 'female'} );
+console.log(maleActors, femaleActors);
+
+// PLUCK ///////////////////////////////////////////////////////////////////////
+var names = _(actors).pluck('name');
+console.log(names);
+
+// MAX /////////////////////////////////////////////////////////////////////////
+var max = _(nums).max();
+console.log('maxiumum', max);
+
+// SHUFFLE /////////////////////////////////////////////////////////////////////
+console.log( _(nums).shuffle() );
+
+// SAMPLE //////////////////////////////////////////////////////////////////////
+console.log( _(nums).sample(3) );
