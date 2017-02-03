1c94916e89d4917ba8c0d23c8e751f33e1e4289d
William of Ockham <certainthings@gmail.com>
2017-02-03 12:50:26 +1100

    unannotated backbone blog

--- 11-backbone/backbone-blog/js/blog.js
+++ 11-backbone/backbone-blog/js/blog.js
@@ -1,6 +1,7 @@
 // MODEL ///////////////////////////////////////////////////////////////////////
 var Post = Backbone.Model.extend({
   defaults: {
+    id: 0,
     author: 'Anonymous',
     title: 'Untitled',
     content: ''
@@ -11,17 +12,19 @@ var Posts = Backbone.Collection.extend({
   model: Post
 });
 
-var p1 = new Post({title: 'My First Post', content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'});
+var p1 = new Post({id: 1, title: 'My First Post', content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'});
 
 // seeds.rb ////////////////////////////////////////////////////////////////////
 // TODO: Move these posts to the Rails server
 var posts = new Posts([
   p1,
   new Post({
+    id: 2,
     title: 'Something About Startups',
     content: 'Something About Mary'
   }),
   new Post({
+    id: 3,
     title: 'Review of Gourmet Hotdogs and Fish Fingers',
     content: 'Something about condiments'
   })
@@ -45,8 +48,7 @@ var PostListView = Backbone.View.extend({
     'click': 'showPost'
   },
   showPost: function () {
-    var postView = new PostView({model: this.model});
-    postView.render();
+    router.navigate('posts/' + this.model.get('id'), true);
   },
   render: function () {
     this.$el.text( this.model.get('title') );
@@ -60,11 +62,28 @@ var PostView = Backbone.View.extend({
     var postTemplate = $('#postTemplate').html();
     var postHTML = _.template(postTemplate);
 
-    this.$el.html( postHTML(this.model.toJSON()) );
+    this.$el.html( postHTML(this.model.attributes) );
   }
 });
 
+// ROUTER //////////////////////////////////////////////////////////////////////
+var Router = Backbone.Router.extend({
+  routes: {
+    '': 'index',
+    'posts/:id': 'show'
+  },
+  index: function () {
+    var appView = new AppView({collection: posts});
+    appView.render();
+  },
+  show: function (id) {
+    var post = posts.get(id);
+    var postView = new PostView({model: post});
+    postView.render();
+  }
+});
+
+var router = new Router();
 $(document).ready(function () {
-  var appView = new AppView({collection: posts});
-  appView.render();
+  Backbone.history.start();
 });
