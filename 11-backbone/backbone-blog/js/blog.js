c10da50226927e112511fa77af08873f20977e13
William of Ockham <certainthings@gmail.com>
2017-02-06 09:25:29 +1100

    annotated backbone blog

--- 11-backbone/backbone-blog/js/blog.js
+++ 11-backbone/backbone-blog/js/blog.js
@@ -1,3 +1,5 @@
+// Backbone equivalents for an ActiveRecord class in Rails.
+// Methods, "columns", validations, event handlers.
 // MODEL ///////////////////////////////////////////////////////////////////////
 var Post = Backbone.Model.extend({
   defaults: {
@@ -8,14 +10,15 @@ var Post = Backbone.Model.extend({
   }
 });
 
+// Like an AR collection giving us methods like .get() and .pluck()
 var Posts = Backbone.Collection.extend({
   model: Post
 });
 
-var p1 = new Post({id: 1, title: 'My First Post', content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'});
-
 // seeds.rb ////////////////////////////////////////////////////////////////////
 // TODO: Move these posts to the Rails server
+var p1 = new Post({id: 1, title: 'My First Post', content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'});
+
 var posts = new Posts([
   p1,
   new Post({
@@ -31,6 +34,9 @@ var posts = new Posts([
 ]);
 
 // VIEWS ///////////////////////////////////////////////////////////////////////
+// Views have two main responsibilities:
+//   present information to the user (usually with HTML)
+//   respond to user interaction (i.e. events)
 var AppView = Backbone.View.extend({
   el: '#main', // jQuery style CSS selector
   render: function () {
@@ -67,6 +73,9 @@ var PostView = Backbone.View.extend({
 });
 
 // ROUTER //////////////////////////////////////////////////////////////////////
+// A mad combination of router and controller
+// The router connects a hash URL (#like_this) to some particular method.
+// The router keeps track of the state of the application.
 var Router = Backbone.Router.extend({
   routes: {
     '': 'index',
@@ -83,7 +92,11 @@ var Router = Backbone.Router.extend({
   }
 });
 
+// We define the router instance as a global variable so we can use it in
+// some of our views.
 var router = new Router();
+
 $(document).ready(function () {
+  // Kicks off the BB routing and enables the browser back and forward buttons.
   Backbone.history.start();
 });
