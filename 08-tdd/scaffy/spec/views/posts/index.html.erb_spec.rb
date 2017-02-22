2c5cb295e42d8619ba179935bfc504a1308d34b5
William of Ockham <certainthings@gmail.com>
2017-02-22 15:05:01 +1100

    rails scaffolding with rspec

---
+++ 08-tdd/scaffy/spec/views/posts/index.html.erb_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe "posts/index", type: :view do
+  before(:each) do
+    assign(:posts, [
+      Post.create!(
+        :title => "MyText",
+        :author => "MyText",
+        :content => "MyText"
+      ),
+      Post.create!(
+        :title => "MyText",
+        :author => "MyText",
+        :content => "MyText"
+      )
+    ])
+  end
+
+  it "renders a list of posts" do
+    render
+    assert_select "tr>td", :text => "MyText".to_s, :count => 2
+    assert_select "tr>td", :text => "MyText".to_s, :count => 2
+    assert_select "tr>td", :text => "MyText".to_s, :count => 2
+  end
+end
---
+++ 08-tdd/scaffy/spec/views/posts/show.html.erb_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe "posts/show", type: :view do
+  before(:each) do
+    @post = assign(:post, Post.create!(
+      :title => "MyText",
+      :author => "MyText",
+      :content => "MyText"
+    ))
+  end
+
+  it "renders attributes in <p>" do
+    render
+    expect(rendered).to match(/MyText/)
+    expect(rendered).to match(/MyText/)
+    expect(rendered).to match(/MyText/)
+  end
+end
