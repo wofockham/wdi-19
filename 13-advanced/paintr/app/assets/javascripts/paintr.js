8dc6d19f68961f30168273f0e3db82b2057eaa60
William of Ockham <certainthings@gmail.com>
2017-03-01 15:14:59 +1100

    paintr with event delegation

--- 13-advanced/paintr/app/assets/javascripts/paintr.js
+++ 13-advanced/paintr/app/assets/javascripts/paintr.js
@@ -1,6 +1,22 @@
 $(document).ready(function () {
   $('#add-color').on('click', function () {
     var color = $('#color').val();
-    console.log( color );
+    var $swatch = $('<div/>').addClass('swatch').css('background-color', color);
+    $swatch.appendTo('.palette');
   });
+
+  // Event delegation
+  // We can listen for events on elements that haven't been created yet.
+  $('.palette').on('click', '.swatch', function () {
+    $('.selected').removeClass('selected');
+    $(this).addClass('selected');
+  });
+
+  // Here we use event delegation to reduce the number of event handlers.
+  $('.canvas').on('mouseover', '.pixel', function (e) {
+    console.log(e);
+    var color = $('.selected').css('background-color');
+    $(this).css('background-color', color);
+  });
+
 });
